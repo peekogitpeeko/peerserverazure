cf8ffe4ed4b70f00cc277b8fd94e1133a8b0686c
shyam <shyam90995@gmail.com>
2023-05-21 16:18:33 +0100

    9th commit

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const port = process.env.PORT || 3000
-const cors = require('cors');
+// const express = require('express');
+// const port = process.env.PORT || 3000
+const port1 = process.env.PORT || 9000
+// const cors = require('cors');
 const { PeerServer } = require("peer");
-const peerServer = PeerServer({ port: 9000, path: "/myapp" });
-const app = express();
+const peerServer = PeerServer({ port: port1, path: "/myapp" });
+// const app = express();
 
-app.use(cors());
+// app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send({"item": "hello world shyam"});
-  });
+// app.get('/', (req, res) => {
+//     res.send({"item": "hello world shyam"});
+//   });
 
-// const port = 3000; // Choose a port number
-
-app.listen(port, () => {
-  console.log(`Server is running1`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running1`);
+// });
 
